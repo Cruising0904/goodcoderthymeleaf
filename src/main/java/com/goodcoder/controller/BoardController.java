bd494a70db4a00c7cd0a8f309cea1c4c2ef037fb
Yong <cucrn35@gmail.com>
2021-11-30 02:50:56 +0900

    validated

--- src/main/java/com/goodcoder/controller/BoardController.java
+++ src/main/java/com/goodcoder/controller/BoardController.java
@@ -2,14 +2,20 @@ package com.goodcoder.controller;
 
 import java.util.List;
 
+import javax.validation.Valid;
+
 import com.goodcoder.model.Board;
 import com.goodcoder.repository.BoardRepository;
+import com.goodcoder.validator.BoardValidator;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/board")
@@ -17,15 +23,34 @@ public class BoardController {
     @Autowired
     private BoardRepository boardRepository;
     
+    @Autowired
+    private BoardValidator boardValidator;
+
     @GetMapping("/list")
     public String list(Model model){
         List<Board> boards = boardRepository.findAll();
         model.addAttribute("boards", boards);
-        System.out.println(boards);
         return "board/list";
     }
-    @GetMapping("/test")
-    public String test(){
-        return "test";
+
+    @GetMapping("/form")
+    public String form(Model model, @RequestParam(required = false) Long id){
+        if(id ==null){
+            model.addAttribute("board", new Board());
+        }else{
+            Board board = boardRepository.findById(id).orElse(null);
+            model.addAttribute("board", board);
+        }
+        return "/board/form";
+    }
+
+    @PostMapping("/form")
+    public String grettingSubmit(@Valid Board board, BindingResult bindingResult){
+        boardValidator.validate(board, bindingResult);
+        if(bindingResult.hasErrors()){
+            return "board/form";
+        }
+        boardRepository.save(board);
+        return "redirect:/board/list";
     }
 }
