ad4c5a882a7cd2645c6173edb4fe8a407507b92e
Yong <cucrn35@gmail.com>
2021-11-30 20:32:22 +0900

    page href

---
+++ readme.md
@@ -0,0 +1,7 @@
+@Valid 자체기능이 작동 안한다... 어디서 잘못된건지 못찾음.
+그래서 @Size(min=2, max=10) 요게 작동 안함.
+
+findByTitleOrContent 적용  --> BoardRepository
+
+Paging
+
--- src/main/java/com/goodcoder/controller/BoardController.java
+++ src/main/java/com/goodcoder/controller/BoardController.java
@@ -1,7 +1,5 @@
 package com.goodcoder.controller;
 
-import java.util.List;
-
 import javax.validation.Valid;
 
 import com.goodcoder.model.Board;
@@ -9,6 +7,9 @@ import com.goodcoder.repository.BoardRepository;
 import com.goodcoder.validator.BoardValidator;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -27,8 +28,14 @@ public class BoardController {
     private BoardValidator boardValidator;
 
     @GetMapping("/list")
-    public String list(Model model){
-        List<Board> boards = boardRepository.findAll();
+    public String list(Model model, @PageableDefault(size =2) Pageable pageable){
+        Page<Board> boards = boardRepository.findAll(pageable);
+        // boards.getTotalElements(); 타임리프에서 가져올거기 때문에 없어도 됨.
+        int startPage = Math.max(1, boards.getPageable().getPageNumber() - 4);
+        int endPage = Math.min(boards.getTotalPages(), boards.getPageable().getPageNumber()+4);
+        
+        model.addAttribute("startPage", startPage);
+        model.addAttribute("endPage", endPage);
         model.addAttribute("boards", boards);
         return "board/list";
     }
